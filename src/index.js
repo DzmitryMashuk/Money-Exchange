1df9a4d38f7474f0dd23dc64dd45a8eb63ab4756
DzmitryMashuk <d.mashuk@itransition.com>
2018-09-19 11:54:08 +0300

    Adding makeExchange method.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	let result = {};
+
+	if (0 >= currency) {
+		return {};
+	}
+
+	if (10000 < currency) {
+		return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	}
+
+	if (0 < Math.floor(currency / 50)) {
+		result.H = Math.floor(currency / 50);
+		currency -= result.H * 50;
+	}
+
+	if (0 < Math.floor(currency / 25)) {
+		result.Q = Math.floor(currency / 25);
+		currency -= result.Q * 25;
+	}
+
+	if (0 < Math.floor(currency / 10)) {
+		result.D = Math.floor(currency / 10);
+		currency -= result.D * 10;
+	}
+
+	if (0 < Math.floor(currency / 5)) {
+		result.N = Math.floor(currency / 5);
+		currency -= result.N * 5;
+	}
+
+	if (0 < currency) {
+		result.P = currency;
+	}
+
+	return result;
 }
